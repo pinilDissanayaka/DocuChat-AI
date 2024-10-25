acdb0b4d534f8270b91aaa59bb7c4c13dab02156
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-25 10:14:48 +0530

    add loader

--- pages/chat.py
+++ pages/chat.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import SimpleDirectoryReader
 
 
 st.set_page_config(page_title="DocuChat AI: Your Intelligent Document Assistant", page_icon="📄", layout="wide")
